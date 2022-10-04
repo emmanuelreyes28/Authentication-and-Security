b25475350693d9cb325b265520750019fb5d8b51
emmanuelreyes28 <63267318+emmanuelreyes28@users.noreply.github.com>
2022-10-03 23:07:42 -0700

    Level 2 - Database Encryption

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const bodParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -12,10 +13,17 @@ app.use(bodParser.urlencoded({extended: true}));
 
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
 
-const userSchema = {
+//need to create a new mongoose.Schema when using encryption
+const userSchema = new mongoose.Schema({
     email: String,
     password: String
-};
+});
+
+const secret = "Thisisourlittlesecret."
+//use encrypt plugin and only encrypt password
+//make sure that encryptedfields matches the same name in schema fields
+userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"]});
+
 
 const User = new mongoose.model("User", userSchema);   
 
